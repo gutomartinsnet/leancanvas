ae4a0efd87f105ab3fe5f67172d67996a03526b8
toywonder <toy.wonder70@gmail.com>
2018-03-05 22:22:22 +0900

    tw_login_comp

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,53 @@ class User < ApplicationRecord
     end
   end
 
+  # def self.new_with_session(params, session)
+  #   if session["devise.user_attributes"]
+  #     new(session["devise.user_attributes"]) do |user|
+  #       user.attributes = params
+  #     end
+  #   else
+  #     super
+  #   end
+  # end
+
+  # def self.from_omniauth(auth)
+  #   # providerとuidでUserレコードを取得する
+  #   # 存在しない場合は、ブロック内のコードを実行して作成する
+  #   where(auth.slice(:provider, :uid)).first_or_create do |user|
+  #     # auth.provider には "twitter"、
+  #     # auth.uidには twitterアカウントに基づいた個別のIDが入っている
+  #     # first_or_createメソッドが自動でproviderとuidを設定してくれるので、
+  #     # ここでは設定は必要ない
+  #     user.username = auth.info.nickname # twitterで利用している名前が入る
+  #     user.email = auth.info.email # twitterの場合入らない
+  #   end
+  # end
+
   def self.new_with_session(params, session)
     if session["devise.user_attributes"]
       new(session["devise.user_attributes"]) do |user|
         user.attributes = params
+        user.valid?
       end
     else
       super
     end
   end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+    # プロフィールを変更するときによばれる
+  def update_with_password(params, *options)
+    # パスワードが空の場合
+    if encrypted_password.blank?
+      # パスワードがなくても更新できる
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+
 end
