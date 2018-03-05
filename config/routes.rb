fc263ef36fb3903d31d98b9d120652a48a4992d8
toywonder <toy.wonder70@gmail.com>
2018-03-05 19:00:16 +0900

    tw_login 途中

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'users/show'
+
+  root to: 'users#show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
