8a79d5f2506f3992519b0ea09d5f44ea0128139f
toywonder <toy.wonder70@gmail.com>
2018-03-20 01:37:47 +0900

    meta-tagsの追加完了

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'pry-byebug',group: [:development,:test]
 gem 'hirb'         # 出力結果を表として出力するgem
 gem 'hirb-unicode'  # マルチバイト文字の表示を補正するgem
 
+gem 'meta-tags' #meta-tags
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'リーンキャンバスメーカー',
+      title: 'LeanCanvasMaker',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Web上でLeanCanvasを描くことが出来る。Webで作成後、画像が生成され、画像の保存が可能',
+      keywords: 'LeanCanvasMaker,LeanCanvas,リーンキャンバス ',
+      canonical: request.original_url,
+      separator: '|',
+      # icon: [
+      #   { href: image_url('favicon.ico') },
+      #   { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      # ],
+      og: {
+        site_name: 'リーンキャンバスメーカー',
+        title: 'LeanCanvasMaker',
+        description: 'Web上でLeanCanvasを描くことが出来る。Webで作成後、画像が生成され、画像の保存が可能',
+        type: 'website',
+        url: request.original_url,
+        # image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+        site: '@RestartFromCafe',
+      }
+    }
+  end
 end
